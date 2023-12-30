19adef3b66ad062370892f554eedc068cb8433c1
Chase <wfp_chase@163.com>
2023-12-30 12:43:51 +0800

    重新部署

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,17 +11,17 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  siteUrl: 'https://run.5iwork.com',
+  logo: 'https://github.com/wwwwwfp/running_page/blob/master/src/images/head.jpg',
   description: 'Personal site and blog',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://blog.5iwork.com',
     },
     {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      name: 'Domain',
+      url: 'http://domain.5iwork.com/',
     },
   ],
 };
