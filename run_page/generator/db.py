569458c83cc878d0906c81279b2de8fcd33a8a7b
Chase <wfp_chase@163.com>
2026-07-19 14:37:11 +0800

    python version 3

--- run_page/generator/db.py
+++ run_page/generator/db.py
@@ -106,7 +106,7 @@ def update_or_create_activity(session, run_activity):
             start_point = run_activity.start_latlng
             location_country = getattr(run_activity, "location_country", "")
             # or China for #176 to fix
-            if not location_country and start_point or (location_country == "China"  or  location_country == "Hong Kong"):
+            if not location_country and start_point or location_country == "China":
                 try:
                     location_country = str(
                         g.reverse(
